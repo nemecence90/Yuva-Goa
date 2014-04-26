7b70b83dff186d5ad640a4e044e4fa7a6ce4ae97
nemecence90 <nemecence90@gmail.com>
2014-04-26 13:47:02 +0530

    first commit

---
+++ HomeViewcontroller.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewcontrollerViewController.h
+//  Yuva-Goa
+//
+//  Created by Abhishek Shah on 26/04/14.
+//  Copyright (c) 2014 Abhishek Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewcontrollerViewController : UIViewController
+
+@end
---
+++ HomeViewcontroller.m
@@ -0,0 +1,38 @@
+//
+//  HomeViewcontrollerViewController.m
+//  Yuva-Goa
+//
+//  Created by Abhishek Shah on 26/04/14.
+//  Copyright (c) 2014 Abhishek Shah. All rights reserved.
+//
+
+#import "HomeViewcontrollerViewController.h"
+
+@interface HomeViewcontrollerViewController ()
+
+@end
+
+@implementation HomeViewcontrollerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
