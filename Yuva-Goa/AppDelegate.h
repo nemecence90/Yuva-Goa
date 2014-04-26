d1ff4a5d9fd1903542956896d0367b84329f626f
nemecence90 <nemecence90@gmail.com>
2014-04-26 13:00:25 +0530

    Initial Commit

---
+++ Yuva-Goa/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Yuva-Goa
+//
+//  Created by Abhishek Shah on 26/04/14.
+//  Copyright (c) 2014 Abhishek Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Yuva-Goa/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Yuva-Goa
+//
+//  Created by Abhishek Shah on 26/04/14.
+//  Copyright (c) 2014 Abhishek Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
